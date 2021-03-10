98075ccc7db140828320169a16155ce8fa41d040
Benjamin <adolphe906@gmail.com>
2021-03-10 14:38:41 +0100

    Création d'un fichier xml puis test requête sur le modèle

--- src/main/java/requetes.java
+++ src/main/java/requetes.java
@@ -1,6 +1,10 @@
 import com.hp.hpl.jena.util.FileManager;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDF;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class requetes {
@@ -37,10 +41,45 @@ public class requetes {
         }
     }
 
-    public static void main(String[] args) {
+    public Model getModel() {
+        return model;
+    }
+
+    public void writeModel() throws IOException {
+        FileWriter RDFXMLOUT = new FileWriter("src/main/resources/conferences.xml");
+        try {
+            this.model.write(RDFXMLOUT);
+        } finally {
+            try {
+                RDFXMLOUT.close();
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         String DataSet = "C:\\Users\\adolp\\OneDrive\\Bureau\\Fac\\Master\\M1\\S2\\Web-Sémantique\\Projet\\projetJena\\src\\main\\resources\\Conferences";
         InputStream in = FileManager.get().open(DataSet);
         Model model = ModelFactory.createDefaultModel();
         model.read(in, null, "RDF/XML");
+        requetes req = new requetes(model);
+        String req_1 = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX conf: <http://www.semanticweb.org/adolp/ontologies/2021/Conference#>"
+                + "SELECT ?nomConf " +
+                "WHERE {?s rdf:type conf:Conférence ." +
+                "?s conf:nomConf ?nomConf .}";
+        Query q = QueryFactory.create(req_1);
+        QueryExecution qexec = QueryExecutionFactory.create(q, req.getModel());
+        try {
+
+            ResultSet rs = qexec.execSelect();
+
+            ResultSetFormatter.out(System.out, rs, q);
+
+        } finally {
+
+            qexec.close();
+        }
     }
 }
